d7c9fb90915e4a1fc9fc5058283d3a2e94396537
Evgeniy <evgeniy.ostapenko.1991@mail.ru>
2019-11-19 17:48:40 +0300

    eight commit.

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -3,6 +3,7 @@ package quoters;
 import javax.annotation.PostConstruct;
 
 @Profiling
+@DeprecatedClass(newImpl = T1000.class)
 public class TerminatorQuoter implements Quoter {
 
     @InjectRandomInt(min = 2, max = 7)
@@ -24,7 +25,7 @@ public class TerminatorQuoter implements Quoter {
     }
 
     @Override
-    @PostProxy
+//    @PostProxy
     public void sayQuote() {
         System.out.println("Phase 3");
         for (int i = 0; i < repeat; i++) {
