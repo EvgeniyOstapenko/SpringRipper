ae2724bf19cb1b70931660b028b8de8e9aaf1039
Evgeniy <evgeniy.ostapenko.1991@mail.ru>
2019-11-19 14:36:29 +0300

    fifth commit.

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -5,9 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        while (true) {
-            Thread.sleep(1000);
-            context.getBean(TerminatorQuoter.class).sayQuote();
-        }
+        context.getBean(Quoter.class).sayQuote();
+
     }
 }
